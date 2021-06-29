7bdf2a7cbea4999415dcc15076125a5200e9e5a2
Hong <fheldgktpdy@khu.ac.kr>
2021-06-29 23:49:11 +0900

    Chap2

---
+++ Chapter2/BsearchRecur.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int BsearchRecur(int arr[], int first, int last, int target) {
+	if (first > last)
+		return -1;
+	int mid = (first + last) / 2;
+	if (arr[mid] == target)
+		return mid;
+	else if (arr[mid] < target) {
+		BsearchRecur(arr, mid+1, last, target);
+	}
+	else
+		BsearchRecur(arr, first, mid-1, target);
+}
---
+++ Chapter2/Fibo.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Fibonnaci(int n) {
+	if (n == 1)
+		return 0;
+	else if (n == 2)
+		return 1;
+	else {
+		return Fibonnaci(n - 2) + Fibonnaci(n - 1);
+	}
+}
+
+int main() {
+	int i;
+	for (i = 1; i < 10; i++) {
+		cout << Fibonnaci(i);
+	}
+}
