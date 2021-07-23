491762a4f8b89439f1a54dc4208ba4c79dbe1365
Hong <fheldgktpdy@khu.ac.kr>
2021-07-24 01:42:37 +0900

    chap4

---
+++ Chapter4/LinkedList.c
@@ -0,0 +1,47 @@
+#include "../Data_Structure_Src/Ch04/DLinkedListSort/DLinkedList.h"
+#include <cstddef>
+void ListInit(List* plist) {
+	plist->head = (Node*)malloc(sizeof(Node));
+	plist->head->next = NULL;
+	plist->comp = NULL;
+	plist->numOfData = 0;
+}
+
+void LInsert(List* plist, LData data) {
+	if (plist->comp == NULL)
+		FInsert(plist, data);
+	else
+		SInsert(plist, data);
+}
+
+int LFirst(List* plist, LData* pdata) {
+	if (plist->head->next == NULL)
+		return FALSE;
+	plist->before = plist->head;
+	plist->cur = plist->head->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+int LNext(List* plist, LData* pdata) {
+	if (plist->cur->next == NULL)
+		return FALSE;
+	plist->before = plist->cur;
+	plist->cur = plist->cur->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+LData LRemove(List* plist) {
+	Node* rpos = plist->cur;
+	LData rdata = rpos->data;
+
+	plist->before->next = plist->cur->next;
+	plist->cur = plist->before;
+
+	free(rpos);
+	(plist->numOfData)--;
+	return rdata;
+}
